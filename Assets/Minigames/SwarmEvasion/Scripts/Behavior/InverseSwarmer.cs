bcc318d6af0e6be6f4ef38e143a3f47ae495fc3c
mcdonaldduncan <380939@colum.edu>
2022-06-24 21:57:53 -0500

    update

--- Assets/Minigames/SwarmEvasion/Scripts/Behavior/InverseSwarmer.cs
+++ Assets/Minigames/SwarmEvasion/Scripts/Behavior/InverseSwarmer.cs
@@ -11,7 +11,7 @@ public class InverseSwarmer : MonoBehaviour
     [SerializeField] float minForce;
     [SerializeField] float baseStrength;
 
-    Vector2 velocity;
+    [SerializeField] Vector2 velocity;
     Vector2 acceleration;
 
     void Update()
@@ -23,21 +23,24 @@ public class InverseSwarmer : MonoBehaviour
     {
         acceleration += CalculateForce();
         velocity += acceleration;
-        velocity = Vector2.ClampMagnitude(velocity, maxSpeed);
+        //velocity = Vector2.ClampMagnitude(velocity, maxSpeed);
         transform.position += (Vector3)velocity * Time.deltaTime;
         acceleration = Vector2.zero;
     }
 
     Vector2 CalculateForce()
     {
+        
         Vector2 desired = target.position - transform.position;
         float distance = desired.magnitude;
+        //float tempForce = maxForce * distance;
         desired.Normalize();
-        //float strength = Mathf.Clamp(distance, minForce, maxForce);
-        float strength = distance * baseStrength;
-        //desired *= strength;
+        desired *= maxSpeed;
+        //float strength = distance * baseStrength;
         Vector2 steer = desired - velocity;
-        steer = steer.normalized * strength;
+        steer.Normalize();
+        steer *= maxForce;
+        //steer *= tempForce;
         return steer;
     }
 
